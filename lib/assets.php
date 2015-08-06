bb6bc77d38af46b08daa18da7fa40cb18213fdca
Eric Imthorn <eric@schitterend.nl>
2015-08-06 21:52:18 +0200

    Altered Sage for personal preference

--- lib/assets.php
+++ lib/assets.php
@@ -71,8 +71,6 @@ function assets() {
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
-
-  wp_enqueue_script('modernizr', asset_path('scripts/modernizr.js'), [], null, true);
   wp_enqueue_script('sage/js', asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
